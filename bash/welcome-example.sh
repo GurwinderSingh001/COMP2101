d9178739fa36aaeedd01100d2ddae90ead6f4481
GurwinderSingh001 <200394547@student.georgianc.on.ca>
2020-01-28 20:55:24 -0500

    123

--- bash/welcome-example.sh
+++ bash/welcome-example.sh
@@ -4,10 +4,10 @@
 # it should look like
 #   Welcome to planet hostname, title name!
 
-# Task 1: Use the variable $USER instead of the myaccount variable to get your name
+# Task 1: Use the variable $USER instead of the name variable to get your name
 # Task 2: Dynamically generate the value for your hostname variable using the hostname command - e.g. $(hostname)
 # Task 3: Add the time and day of the week to the welcome message using the format shown below
-#   Use a format like this:
+#   Use a format like this:-
 #   It is weekday at HH:MM AM.
 # Task 4: Set the title using the day of the week
 #   e.g. On Monday it might be Optimist, Tuesday might be Realist, Wednesday might be Pessimist, etc.
@@ -17,8 +17,7 @@
 ###############
 # Variables   #
 ###############
-title="Overlord"
-myaccount="$USER"
+name="$USER"
 hostname="$(hostname)"
 day=$(date +%A)
 currenttime=$(date +%I:%M\ %p)
@@ -27,23 +26,23 @@ currenttime=$(date +%I:%M\ %p)
 ###############
 if [ "$day" = Monday ]
 then
-echo "Welcome to planet $hostname, Adaptable $myaccount!"
+echo "Welcome to planet $hostname, Great $name!"
 elif [ "$day" = Tuesday ]
 then
-echo "Welcome to planet $hostname, Courteous $myaccount!"
+echo "Welcome to planet $hostname, Good $name!"
 elif [ "$day" = Wednesday ]
 then
-echo "Welcome to planet $hostname, Adaptable $myaccount!"
+echo "Welcome to planet $hostname, Knowledgable $name!"
 elif [ "$day" = Thursday ]
 then
-echo "Welcome to planet $hostname, Frank $myaccount!"
+echo "Welcome to planet $hostname, IT man $name!"
 elif [ "$day" = Friday ]
 then
-echo "Welcome to planet $hostname, Impartial $myaccount!"
+echo "Welcome to planet $hostname, Techy $name!"
 elif [ "$day" = Saturday ]
 then
-echo "Welcome to planet $hostname, Inventive $myaccount!"
+echo "Welcome to planet $hostname, Assistant $name!"
 else
-echo "Welcome to planet $hostname, Resourceful $myaccount!"
+echo "Welcome to planet $hostname, Professor $name!"
 fi
 echo "It is $currenttime on $day ."
