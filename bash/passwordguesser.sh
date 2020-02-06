eaee76adc8fd6fe700de4a113a025f244cb06578
GurwinderSingh001 <200394547@student.georgianc.on.ca>
2020-02-05 21:19:48 -0500

    love FN

--- bash/passwordguesser.sh
+++ bash/passwordguesser.sh
@@ -25,12 +25,28 @@ else
   echo
   echo "Incorrect username or password!!"
   read -s -p "Guess the password:" myString
+  if [ $myString = $referenceString ]; then
+    echo "Login Successful."
+    echo
+  else
+    echo
+    echo "Incorrect username or password!!"
+    read -s -p "Guess the password:" myString
+    if [ $myString = $referenceString ]; then
+      echo "Login Successful."
+      echo
+    else
+      echo
+      echo "Incorrect username or password!!"
+      read -s -p "Guess the password:" myString
 if [ $myString = $referenceString ]; then
   echo "Login Successful."
   echo
 else
   echo
   echo "Incorrect username or password!!"
+fi
+fi
 fi
 fi
 fi
