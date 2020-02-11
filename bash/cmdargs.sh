7284aa8a11627b1ce44cfea384eb2af15f4217e3
GurwinderSingh001 <200394547@student.georgianc.on.ca>
2020-02-11 12:10:41 -0500

    done

--- bash/cmdargs.sh
+++ bash/cmdargs.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-# This script demonstrates how the shift command works
-# create an empty array to put the command line arguments into
+#
+#
+#
 myargs=()
-# loop through the command line arguments
+#
 while [ $# -gt 0 ]; do
-  # tell the user how many things are left on the command line
+  #
   echo "There are $# things left to process on the command line."
-  # add whatever is in $1 to the myargs array
+  #
   myargs+=("$1")
-  # tell the user what we did
+  #
   echo "Added \'$1\' to the arguments array"
   echo "Processing '$1'."
   case $1 in
@@ -34,12 +34,12 @@ while [ $# -gt 0 ]; do
     echo "Error: unkown value $errors"
     ;;
     esac
-  # each time through the loop, shift the arguments left
-  # this decrements the argument count for us
+  #
+  #
   shift
-  # tell the user we shifted things
+  #
   echo "Shifted command line, leaving $# things left to process."
   echo "--------------------------"
-  # go back to the top of the loop to see if anything is left to work on
+  # 
 done
 echo "Done"
